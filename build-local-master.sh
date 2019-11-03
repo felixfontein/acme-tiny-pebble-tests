a2a33f8285b80a57774c673b93fad45f3f539fae
Felix Fontein <felix@fontein.de>
2019-11-03 15:12:47 +0100

    Bump Golang base container.

--- build-local-master.sh
+++ build-local-master.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# docker pull golang:1.10-stretch
+# docker pull golang:1.13-stretch
 # docker pull python:3.6-slim-stretch
 docker image build --network host --build-arg PEBBLE_CHECKOUT=master -t local/acme-tiny-pebble-test-container:latest docker-container
